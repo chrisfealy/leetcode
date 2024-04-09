b53349b76f2c8604977df35050721cb4312c259e
chrisfealy <chrismfealy@gmail.com>
2024-04-09 10:37:37 -0500

    contains duplicate python solution

--- python/0217-contains-duplicate.py
+++ python/0217-contains-duplicate.py
@@ -1,8 +1,11 @@
 class Solution(object):
     def containsDuplicate(self, nums):
         dupes = set()
+
         for num in nums:
             if num in dupes:
                 return True
+
             dupes.add(num)
+
         return False
