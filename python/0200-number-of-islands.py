bc5f203749df6b94112a4f0edd73ee426692ff39
chrisfealy <chrismfealy@gmail.com>
2024-04-29 22:10:27 -0500

    number of islands python solution

--- python/0200-number-of-islands.py
+++ python/0200-number-of-islands.py
@@ -1,22 +1,26 @@
+// bfs approach
 class Solution(object):
     def numIslands(self, grid):
         rows, cols = len(grid), len(grid[0])
         visited = set()
         islands = 0
+
         def bfs(i, j):
             queue = [[i, j]]
             visited.add((i, j))
             while queue:
-                nrow, ncol = queue.pop(0)
+                row, col = queue.pop(0)
                 directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
                 for dr, dc in directions:
-                    row, col = nrow + dr, ncol + dc
-                    if row in range(rows) and col in range(cols) and grid[row][col] == '1' and (row, col) not in visited:
-                        queue.append([row, col])
-                        visited.add((row, col))
+                    new_row, new_col = dr + row, dc + col
+                    if new_row in range(rows) and new_col in range(cols) and grid[new_row][new_col] == '1' and (new_row, new_col) not in visited:
+                        queue.append([new_row, new_col])
+                        visited.add((new_row, new_col))
+
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == '1' and (i, j) not in visited:
                     bfs(i, j)
                     islands += 1
+
         return islands
