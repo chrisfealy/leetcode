82bc32a93861210e60be7da6ce3f5d61d9768ba9
chrisfealy <chrismfealy@gmail.com>
2024-05-07 22:21:39 -0500

    balanced binary tree python solution

--- python/0110-balanced-binary-tree.py
+++ python/0110-balanced-binary-tree.py
@@ -8,8 +8,7 @@
 class Solution(object):
     def isBalanced(self, root):
         def dfs(node):
-            if not node:
-                return [True, 0]
+            if not node: return [True, 0]
             left, right = dfs(node.left), dfs(node.right)
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
             return [balanced, 1 + max(left[1], right[1])]
